ef8e8e36bbfbe7581b4bb1541a2958b9eabbe194
Jury Razumau <jurazumau@gmail.com>
2021-12-03 14:22:41 +0100

    complete day 1

--- 01_sonar_sweep.rb
+++ 01_sonar_sweep.rb
@@ -1,3 +1,26 @@
 require_relative "utils"
 
-puts Utils.read_integers("input/01.txt")
+def count_increases(depths)
+  previous = depths.first
+  depths[1..-1].reduce(0) do |count, depth|
+    if depth > previous
+      count += 1
+    end
+    previous = depth
+    count
+  end
+end
+
+
+
+def count_window_increases(depths)
+  windows = (0..depths.size - 3).map do |index|
+    depths[index] + depths[index + 1] + depths[index + 2]
+  end
+  count_increases(windows)
+end
+
+depths = Utils.read_integers("input/01.txt")
+
+puts count_increases(depths)
+puts count_window_increases(depths)
